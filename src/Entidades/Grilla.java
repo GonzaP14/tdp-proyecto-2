7c96f25f6a32f50e5aaf13137461136b98e13c9b
Brun0Mand0lesi <brunomandol@gmail.com>
2021-09-16 18:49:01 -0300

    commit grilla y logica

--- src/Entidades/Grilla.java
+++ src/Entidades/Grilla.java
@@ -1,5 +1,64 @@
 package Entidades;
 
-public class Grilla {
 
-}
+	import java.awt.Color;
+
+	public class Grilla {
+	    //Atributos
+	    private Color grilla[][];
+
+	    public Grilla() {
+	        grilla = new Color[21][10];
+	        for(int filas=0;filas<21;filas++) {
+	        	for(int columnas=0;columnas<10;columnas++) {
+	        		grilla[filas][columnas]=new Color(0,0,0);
+	        	}
+	        }
+	    }
+
+	    //Buscar Colisiones
+	    public boolean buscarColisiones(Tetrimino t) {
+	    	boolean retorno=false;
+	    	float[] enCoordenada=null;
+	    	Color negro=new Color(0,0,0);
+	    	
+	    	for(int i=0;i<4;i++) {
+	    		if(t.dondeMoverIzquierda()[i]==null || t.dondeMoverDerecha()[i]==null || t.dondeMoverAbajo()==null) {
+	    			retorno=true;
+	    		}
+	    		if( !grilla[t.dondeMoverIzquierda()[i].getX()][t.dondeMoverIzquierda()[i].getY()].getColorComponents(enCoordenada).equals(negro) || !grilla[t.dondeMoverDerecha()[i].getX()][t.dondeMoverDerecha()[i].getY()].getColorComponents(enCoordenada).equals(negro) || !grilla[t.dondeMoverAbajo()[i].getX()][t.dondeMoverAbajo()[i].getY()].getColorComponents(enCoordenada).equals(negro)) {
+	    			retorno= true;
+	    		}
+	    	}
+	    	return retorno;
+	    }
+
+	    //Evaluar linea
+	    public boolean evaluarLinea(int x) {
+	    	
+	    	boolean aEliminar=false;
+	    	float[] enCoordenada=null;
+	    	Color negro=new Color(0,0,0);
+	    	
+	    	for(int columnas=0;columnas<10;columnas++) {
+	    		if(grilla[x][columnas].getColorComponents(enCoordenada).equals(negro) ) {
+	    			aEliminar=true; 
+	    		}
+	    	}
+	    	return aEliminar;
+	    }
+
+	    //OcuparCelda
+	    public void  ocuparCelda(Tetrimino t) {
+	    	Par[]posiciones=t.getPosicionesCuadrado();
+	    	Color algo=null;
+	    	
+	    	for(int i=0;i<4;i++) {
+	    		grilla[posiciones[i].getX()][posiciones[i].getY()]=t.getColor();
+	    	}
+	    }
+	    
+	    //Obtener color de celda
+
+	}
+
--- src/Logica/Logica.java
+++ src/Logica/Logica.java
@@ -1,5 +1,67 @@
 package Logica;
 
 public class Logica {
+    //Atributos
+    private int nivelActual,puntajeActual;
+    private boolean estaPausado;
 
+    //Constructor
+    public Logica() {
+        nivelActual = 0;
+        puntajeActual = 0;
+        estaPausado = false;
+    }
+
+    //Comandos
+    public void pausar() {
+        estaPausado = true;
+    }
+
+    public void reanudar() {
+        estaPausado = false;
+    }
+
+    public void guardarBloque() {
+
+    }
+
+    private void borrarLinea() {
+
+    }
+
+    public void aumentarPuntaje() {
+
+    }
+
+    public void bajarForma() {
+
+    }
+
+    public void moverIzq() {
+
+    }
+
+    public void moverDer() {
+
+    }
+
+    public void intercambiarTetrimonio() {
+
+    }
+
+    public void elegirTetrimonioProximo() {
+
+    }
+
+    private void terminarJuego() {
+
+    }
+
+    public void iniciarReloj() {
+
+    }
+
+    public void rotarTetrimonioActual() {
+
+    }
 }
