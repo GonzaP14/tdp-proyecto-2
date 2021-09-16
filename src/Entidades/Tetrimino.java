b5027539ca51dc0b12bca1c12ad124ecb0560b6f
nico23112001 <62406648+nico23112001@users.noreply.github.com>
2021-09-16 16:54:08 -0300

    commit con clase par y tetrimino

--- src/Entidades/Tetrimino.java
+++ src/Entidades/Tetrimino.java
@@ -1,5 +1,22 @@
 package Entidades;
-
-public class Tetrimino {
-
+import java.awt.Color;
+public abstract class Tetrimino {
+	protected int cantidadCuadrados;
+	protected Par[] posicionesCuadrados;
+	protected Color color;
+	protected int rotacionActual;
+	
+	public abstract void rotarDerecha();
+	
+	public abstract void rotarIzquierda();
+	
+	public abstract Par[] dondeMoverIzquierda();
+	
+	public abstract Par[] dondeMoverDerecha();
+	
+	public abstract Par[] dondeMoverAbajo();
+	
+	public abstract Par[] dondeRotarDerecha();
+	
+	public abstract Par[] dondeRotarIzquierda();
 }
