ed56c5a39a575ea75e92675932dce289253c590d
Gonzalo <89589881+GonzaP14@users.noreply.github.com>
2021-09-20 18:50:19 -0300

    commit - Tetrimino

--- src/Entidades/Tetrimino.java
+++ src/Entidades/Tetrimino.java
@@ -1,102 +1,92 @@
-package Entidades;
 import java.awt.Color;
 
 public abstract class Tetrimino {
+
+    // Atributos de clase.
 	
-	// Atributos de clase.
-	
-	protected static final int cantidadCuadrados = 4;
-	
-	// Atributos de instancia.
-	
-	protected Par[] posicionesCuadrados;
-	protected Color color;
-	protected int rotacionActual;
-	
-	/**
-	 * Rota el tetrimino hacia la derecha.
-	 */
-	public abstract void rotarDerecha();
-	
-	/**
-	 * Rota el tetrimino hacia la izquierda.
-	 */
-	public abstract void rotarIzquierda();
-	
-	/**
-	 * Retorna las posiciones a las cuales se trasladaría el tetrimino con un movimiento a izquierda.
-	 * @return Posiciones del tetrimino con movimiento a izquierda.
-	 */
-	public Par[] dondeMoverIzquierda() {
-		Par[] posicionesAMover = new Par[4];
-		for(int i = 0; i < cantidadCuadrados ; i++) {
-			posicionesAMover[i] = new Par( posicionesCuadrados[i].getX() - 1 , posicionesCuadrados[i].getY() );
-		}
-		return posicionesAMover;
-	}
-		
-	/**
-	 * Retorna las posiciones a las cuales se trasladaría el tetrimino con un movimiento a derecha.
-	 * @return Posiciones del tetrimino con movimiento a derecha.
-	 */
-	public Par[] dondeMoverDerecha() {
-		Par[] posicionesAMover = new Par[4];
-		for(int i = 0; i < cantidadCuadrados ; i++) {
-			posicionesAMover[i] = new Par( posicionesCuadrados[i].getX() + 1 , posicionesCuadrados[i].getY() );
-		}
-		return posicionesAMover;
-	}
-	
-	/**
-	 * Retorna las posiciones a las cuales se trasladaría el tetrimino con un movimiento hacia abajo.
-	 * @return Posiciones del tetrimino con movimiento hacia a abajo.
-	 */
-	public Par[] dondeMoverAbajo() {
-		Par[] posicionesAMover = new Par[4];
-		for(int i = 0; i < cantidadCuadrados ; i++) {
-			posicionesAMover[i] = new Par( posicionesCuadrados[i].getX() , posicionesCuadrados[i].getY() - 1 );
-		}
-		return posicionesAMover;
-	}
-	
-	/**
-	 * Retorna las posiciones a las cuales se trasladaría el tetrimino con una rotación a derecha.
-	 * @return Posiciones del tetrimino con rotación a derecha.
-	 */
-	public abstract Par[] dondeRotarDerecha();
-	
-	/**
-	 * Retorna las posiciones a las cuales se trasladaría el tetrimino con una rotación a izquierda.
-	 * @return Posiciones del tetrimino con rotación a izquierda.
-	 */
-	public abstract Par[] dondeRotarIzquierda();
-	
-	/**
-	 * Retorna las posiciones actuales del tetrimino.
-	 * @return Posiciones del tetrimino.
-	 */
-	public abstract Par [] getPosicionesCuadrado();
-		
-	/**
-	 * Retorna el color del tetrimino. 
-	 * @return Color del tetrimino.
-	 */
-	public abstract Color getColor();
-	
-	/**
-	 * Establece el color del tetrimino.
-	 * @param color Color nuevo del tetrimino.
-	 */
-	public abstract void setColor(Color color);
-	
-	/**
-	 * Retorna la rotacion actual del tetrimino.
-	 * @return Rotacion actual del tetrimino.
-	 */
-	public abstract int getRotacion();
-	
-	/**
-	 * Establece la rotación del tetrimino.
-	 * @param rotacionActual Rotacion nueva del tetrimino.
-	 */
+    protected static final int cantidadCuadrados = 4;
+    protected static final Par origenGrilla = new Par(5, 0);
+    
+    // Atributos de instancia.
+    
+    protected Grilla miGrilla;
+    protected Par [][] rotaciones;
+    protected Par [] posicionesActuales;
+    protected Color color;
+    protected int rotacionActual;
+    protected Par centroPieza;
+
+    /*
+     * Rota el tetrimino hacia la derecha.
+     */
+    public boolean rotarDerecha() {
+    	int rotacionSiguiente = (rotacionActual == 3) ? 0 : (rotacionActual + 1) % 4; 
+    	boolean check = !miGrilla.buscarColisiones (centroPieza.getX(), centroPieza.getY(), rotaciones [rotacionSiguiente]);
+    	
+    	if (check) {
+    		posicionesActuales = rotaciones [rotacionSiguiente];
+    	}
+    	
+    	return check;
+    }
+
+    /*
+     * Rota el tetrimino hacia la izquierda.
+     */
+    public boolean rotarIzquierda() {
+    	int rotacionSiguiente = (rotacionActual == 0) ? 3 : (rotacionActual - 1) % 4;
+    	boolean check = !miGrilla.buscarColisiones (centroPieza.getX(), centroPieza.getY(), rotaciones [rotacionSiguiente]);
+    	
+    	if (check) {
+    		posicionesActuales = rotaciones [rotacionSiguiente];
+    	}
+    	
+    	return check;
+ 
+    }
+    
+    /**
+     * Mueve el tetrimino a derecha.
+     * @return
+     */
+    public boolean moverDerecha() {
+        boolean check = !miGrilla.buscarColisiones(centroPieza.getX(), centroPieza.getY(), rotacionActual);
+        
+        if (check) {
+            centroPieza.setX(centroPieza.getX() + 1);
+            check = true;
+        }
+        
+        return check;
+    }
+    
+    /*
+     * Mueve el tetrimino a izquierda.
+     */
+    public boolean moverIzquierda() {
+        boolean check = !miGrilla.buscarColisiones(centroPieza.getX(), centroPieza.getY(), rotacionActual);
+        
+        if (check) {
+            centroPieza.setX(centroPieza.getX() - 1);
+        }
+        
+        return check;
+    }
+
+    /*
+     * Retorna las posiciones actuales del tetrimino.
+     * @return Posiciones del tetrimino.
+     */
+    public Par [] getPosicionesActuales() {
+        return posicionesActuales;
+    }
+
+    /*
+     * Retorna el color del tetrimino. 
+     * @return Color del tetrimino.
+     */
+    public Color getColor() {
+        return color;
+    }
+
 }
