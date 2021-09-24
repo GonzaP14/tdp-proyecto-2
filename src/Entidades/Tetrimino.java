416a218b9a221581e56ab22bf927fa1dad7b1e6b
Gonzalo <89589881+GonzaP14@users.noreply.github.com>
2021-09-24 17:21:33 -0300

    Actualización - Tetrimino

--- src/Entidades/Tetrimino.java
+++ src/Entidades/Tetrimino.java
@@ -17,36 +17,51 @@ public abstract class Tetrimino {
     protected int rotacionActual;
     protected Par centroPieza;
 
-    /*
+    /**
      * Rota el tetrimino hacia la derecha.
+     * @return true si el Tetrimino pudo rotar a derecha y false en caso contrario.
      */
     public boolean rotarDerecha() {
-    	int rotacionSiguiente = (rotacionActual + 1) % 4; 
-    	boolean check = miGrilla.buscarColisiones (centroPieza.getX(), centroPieza.getY(), rotaciones [rotacionSiguiente]);
-    	
-    	if (!check) {
-    		posicionesActuales = rotaciones [rotacionSiguiente];
-    		rotacionActual = rotacionSiguiente;
-    	}
-    	
-    	return !check;
+    	return rotar(1);
     }
 
-    /*
+    /**
      * Rota el tetrimino hacia la izquierda.
+     * @return true si el Tetrimino pudo rotar a izquierda y false en caso contrario.
      */
     public boolean rotarIzquierda() {
-    	int rotacionSiguiente = (rotacionActual == 0) ? 3 : (rotacionActual - 1) ;
-    	boolean check = miGrilla.buscarColisiones (centroPieza.getX(), centroPieza.getY(), rotaciones [rotacionSiguiente]);
-    	
-    	if (!check) {
-    		posicionesActuales = rotaciones [rotacionSiguiente];
-    		rotacionActual = rotacionSiguiente;
-    	}
-    	return !check;
+    	return rotar(-1);
     }
     
     /**
+     * Rota el tetrimino a derecha o izquierda.
+     * @param i Parametro de rotacion: 1 (Rotación a Derecha) y -1 (Rotación a Izquierda).
+     * @return true si el Tetrimino pudo rotar y false en caso contrario.
+     */
+    private boolean rotar(int i) {
+    	int rotacionSiguiente = 0;
+    	boolean check;
+    	
+    	if (i == 1) {
+    		rotacionSiguiente = (rotacionActual + 1) % 4;
+    	} else if (i == -1) {
+    		rotacionSiguiente = (rotacionActual == 0) ? 3 : (rotacionActual - 1) ;
+    	}
+    	
+    	check = miGrilla.buscarColisiones (centroPieza.getX(), centroPieza.getY(), rotaciones [rotacionSiguiente]);
+    	
+    	if (!check) {
+    		posicionesActuales = rotaciones [rotacionSiguiente];
+    		rotacionActual = rotacionSiguiente;
+    	}
+    	
+    	
+    	return (!check);
+    }
+    
+    
+    /**
+     * Mueve el Tetrimino hacia la derecha.
      * @return true si el movimiento a derecha fue posible y false en caso contrario.
      */
     public boolean moverDerecha() {
@@ -54,6 +69,7 @@ public abstract class Tetrimino {
     }
     
     /**
+     * Mueve el Tetrimino hacia la izquierda.
      * @return true si el movimiento a izquierda fue posible y false en caso contrario.
      */
     public boolean moverIzquierda() {
@@ -61,6 +77,7 @@ public abstract class Tetrimino {
     }
     
     /**
+     * Mueve el Tetrimino lateralmente.
      * @param i parametro de desplazamiento: 1 (movimiento a derecha) y -1 (movimiento a izquierda)
      * @return true si el movimiento lateral fue posible y false en caso contrario.
      */
@@ -75,6 +92,7 @@ public abstract class Tetrimino {
     }
     
     /**
+     * Mueve el Tetrimino hacia abajo.
      * @return true si el movimiento hacia abajo fue posible y false en caso contrario.
      */
     public boolean moverAbajo() {
@@ -95,13 +113,18 @@ public abstract class Tetrimino {
         return posicionesActuales;
     }
 
-    /*
-     * @return ImageIcon del Color del tetrimino.
+    /**
+     * Retorna la imagen de color asociada al Tetrimino.
+     * @return Imagen asociada al Tetrimino.
      */
     public ImageIcon getColor () {
     	return color;
     }
  
+    /**
+     * Retorna el par centro del Tetrimino. 
+     * @return Par asociado al centro Tetrimino
+     */
 	public Par getCentroPieza() {
 		return centroPieza;
 	}
